8283212306f536c02573b1a2dbbc7b19f5eae36a
Cesare Montresor <cesare.montresor@gmail.com>
2024-02-04 21:42:46 +0100

    - added foods sprites

--- Godot/Scenes/food.gd
+++ Godot/Scenes/food.gd
@@ -4,17 +4,20 @@ static var food_potency = [1,3,5,11]
 
 @onready var food_sprites = [$food_0, $food_1, $food_2, $food_3]
 
-var food_level = 0
+@export_range(0,3) var food_level = 0
 
 func set_potency(level):
 	if(level>len(food_potency)): level = len(food_potency) - 1
 	if(level<0): level = 0
-	for sprite in food_sprites: sprite.visible = false
-	food_sprites[level].visible = true
-	food_level = level
+	food_potency = level
+	
+func get_potency():
+	return food_potency[food_level]
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	for sprite in food_sprites: sprite.visible = false
+	food_sprites[food_level].visible = true
 	pass # Replace with function body.
 
 
