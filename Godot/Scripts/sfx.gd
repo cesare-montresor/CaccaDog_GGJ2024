5e50c1c761ef15ea8f77e937c2f0421f5f77c081
papataci <registro@eperinelli.com>
2024-02-05 00:15:02 +0100

    Aggiunti primi sfx

---
+++ Godot/Scripts/sfx.gd
@@ -0,0 +1,16 @@
+extends Node
+
+#@export var Accept : AudioStreamPlayer
+#@export var Cancel : AudioStreamPlayer
+#@export var Interact : AudioStreamPlayer
+@export var Fart1 : AudioStreamPlayer
+@export var Fart2 : AudioStreamPlayer
+@export var Fart3 : AudioStreamPlayer
+#@export var Hit : AudioStreamPlayer
+@export var Death : AudioStreamPlayer
+#@export var PickUp : AudioStreamPlayer
+#@export var Win : AudioStreamPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
