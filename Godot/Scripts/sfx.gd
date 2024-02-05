42da9d4f76659f6c047f5c1e4524e5ca83edd509
papataci <registro@eperinelli.com>
2024-02-05 01:13:03 +0100

    Aggiunto SFX Eat

--- Godot/Scripts/sfx.gd
+++ Godot/Scripts/sfx.gd
@@ -3,15 +3,27 @@ extends Node
 #@export var Accept : AudioStreamPlayer
 #@export var Cancel : AudioStreamPlayer
 #@export var Interact : AudioStreamPlayer
+@export var Eat : AudioStreamPlayer
 @export var Fart1 : AudioStreamPlayer
 @export var Fart2 : AudioStreamPlayer
 @export var Fart3 : AudioStreamPlayer
+@export var Fart4 : AudioStreamPlayer
+@export var Fart5 : AudioStreamPlayer
+@export var Fart6 : AudioStreamPlayer
+@export var Fart7 : AudioStreamPlayer
+@export var Fart8 : AudioStreamPlayer
+@export var Fart9 : AudioStreamPlayer
+@export var Fart10 : AudioStreamPlayer
+@export var Fart11 : AudioStreamPlayer
+@export var Fart12 : AudioStreamPlayer
+@export var Fart13 : AudioStreamPlayer
+@export var Fart14 : AudioStreamPlayer
 #@export var Hit : AudioStreamPlayer
 @export var Death : AudioStreamPlayer
 #@export var PickUp : AudioStreamPlayer
 #@export var Win : AudioStreamPlayer
 
-@onready var farts = [Fart1,Fart2,Fart3]
+@onready var farts = [Fart1,Fart2,Fart3,Fart4,Fart5,Fart6,Fart7,Fart8,Fart9,Fart10,Fart11,Fart12,Fart13,Fart14]
 
 func fart():
 	farts.pick_random().play()
