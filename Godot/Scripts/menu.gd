370f5b406f65b95b0c0cd619b3ab0a15f73553f2
papataci <registro@eperinelli.com>
2024-02-11 17:05:48 +0100

    Animazione laterale Dog, sistemato menu

--- Godot/Scripts/menu.gd
+++ Godot/Scripts/menu.gd
@@ -21,7 +21,7 @@ func _on_level_2_pressed():
 
 
 func _on_level_3_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/L03.tscn")
 
 
 func _on_credits_pressed():
