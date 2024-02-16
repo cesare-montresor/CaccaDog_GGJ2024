c1feb561a2bd2ed105f8f2519cf7cddbb363f4a0
papataci <registro@eperinelli.com>
2024-02-16 16:20:32 +0100

    Ridimensionato finestra a 1024 Ridotto scala Aggiunto counter vite, mosche e cacche

--- Godot/src/BaseEnemy.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-
-@export var name : String = "Enemy"
-@export var texture : Texture = null
-@export var health : int = 30
-@export var damage : int = 20
-@export var minXP : int = 5
-@export var maxXP : int = 15
-
