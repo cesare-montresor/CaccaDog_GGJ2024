52f4ce1f25188d36b798a449cd1ca9b07b253c8c
papalagi <registro@eperinelli.com>
2024-02-28 22:22:34 +0100

    Sistemazione livelli

--- Godot/Autoload/game_params.gd
+++ Godot/Autoload/game_params.gd
@@ -12,7 +12,7 @@ const layer_finish = 3
 # Player
 const num_lifes = 3
 const step_speed = 0.200
-const step_delay = 0.022
+const step_delay = 0.015
 const initial_poop_count : int = 0
 
 
@@ -21,9 +21,9 @@ const poops_per_fly : int = 4
 
 const fly_speed : int = 40
 
-const fly_action_poop : float = 0.6
-const fly_action_move : float = 0.4
-const fly_action_follow : float = 1.5
+const fly_action_poop : float = 0.3
+const fly_action_move : float = 0.2
+const fly_action_follow : float = 0.5
 
 const fly_action_cooldown = [1,1]
 
