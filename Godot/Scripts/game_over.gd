e8630836240d6e5dfc4d25467222aa4d7fccb0a1
papalagi <registro@eperinelli.com>
2024-03-12 19:16:04 +0100

    Si parte dall'intro, aggiunto logo TTNC

--- Godot/Scripts/game_over.gd
+++ Godot/Scripts/game_over.gd
@@ -4,8 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# PAUSE
-	await get_tree().create_timer(7.5
-	timeout
+	await get_tree().create_timer(7.5).timeout
 	
 	await $LevelTitle.to_white()
 	GameManager.LoadMenu()
