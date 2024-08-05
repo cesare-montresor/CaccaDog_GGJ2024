bb8a82c3c6445933d1371d613ca14ed2d63d17e5
papalagi <registro@eperinelli.com>
2024-08-05 20:00:46 +0200

    Allargato BG Livelli
    tweak posizione cancello chiuso

---
+++ Godot/Scenes/TransfAnimationPlayer.gd
@@ -0,0 +1,9 @@
+extends AnimationPlayer
+
+@onready var loop_animation_player = $"../LoopAnimationPlayer"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#play("FromLeftToRight")
+	#loop_animation_player.loop_anim("WalkRight")	
---
+++ Godot/Scenes/simplePlay.gd
@@ -0,0 +1,6 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func loop_anim(anim_name : String):
+	play(anim_name)
